cea3a52da743df655ae60da87d978a2a4f3b163b
thomasninane <thomas.ninane@gmail.com>
2021-11-19 15:04:13 +0100

    Add data for companies in S&P500 (from 2012-01-31 to 2017-01-31)

--- download_data.R
+++ download_data.R
@@ -1,17 +1,17 @@
 library(quantmod)
 library(tidyquant)
 
+
 data_dir = "./data"
-data_start = "2017-01-01"
-date_end = "2021-01-01"
+data_start = "2012-01-31"
+date_end = "2017-01-31"
 
 
-sp500 = read.csv(paste(data_dir, "/S&P500.csv", sep=""))
+sp500 = read.csv(paste(data_dir, "/S&P500_SharedConstituents.csv", sep=""))
 print(head(sp500))
 
 for (i in 1:nrow(sp500)){
   symbol = sp500$Symbol[i]
-  print(paste("Downloading and exporting data for:", symbol, "->", i, sep=" "))
   
   tryCatch({
     data = getSymbols(
@@ -21,9 +21,23 @@ for (i in 1:nrow(sp500)){
       warnings = TRUE,
       auto.assign = FALSE
     )
-    write.csv(data, paste(data_dir, "/", symbol, ".csv", sep=""))
+    
+    names(data) = sub(".\\.", "", names(data))
+    
+    write.zoo(
+      data,
+      paste(data_dir, "/stocks/", symbol, ".csv", sep=""),
+    )
+    
+    print(paste("Data downloaded and exported for:", symbol, "->", i, sep=" "))
+    
   }, error = function(e) {
-      print(paste("Error downloading data for:", symbol, sep=" "))
+      print(paste("Error downloading data for:", symbol, "->", i, sep=" "))
     }
   )
 }
+
+# temp1 = read.zoo(
+#   paste(data_dir, "/stocks/A.csv", sep=""),
+#   header = TRUE
+# )
